54019620698c927ae61a15e091954e0a9a19a746
Víctor Lozano <55434666+VictorLozano1780@users.noreply.github.com>
2021-02-02 16:37:15 +0100

    Add files via upload

---
+++ TasquesUD4_3/src/TasquesUD4_3.java
@@ -0,0 +1,46 @@
+
+public class TasquesUD4_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 5;
+		double n = 3.43;
+		double m = 4.76;
+		//Valor variables
+		System.out.println("x = " + x + "\ny=" + y + "\nn= " + n + "\nm= " + m);
+		//Suma x y
+		System.out.println("X + Y = " + (x+y));
+		//Resta x y
+		System.out.println("X - Y= " + (x-y));
+		//Producto x y
+		System.out.println("X * Y= " + (x*y));
+		//Cociente x y
+		System.out.println("X / Y= " + (x/y));
+		//Resto x y
+		System.out.println("X % Y= " + (x%y));
+		//Suma n m
+		System.out.println("N + M= " + (n+m));
+		//Resta n m
+		System.out.println("N - M= " + (n-m));
+		//Producto n m
+		System.out.println("N * M= " + (n*m));
+		//Cociente n m
+		System.out.println("N / M= " + (n/m));
+		//Resto n m
+		System.out.println("N % M= " + (n%m));
+		//Suma x n
+		System.out.println("X + N= " + (x+n));
+		//Cociente y m
+		System.out.println("Y / M= " + (y+m));
+		//Resto y m
+		System.out.println("Y % M= " + (y%m));
+		//Doble de todas
+		System.out.println("x*2= " + (x*2) + "\ny*2= " + (y*2) + "\nn*2= " + (n*2) + "\nm*2= " + (m*2));
+		//Suma de todos
+		System.out.println("Suma de todos= " + (x+y+n+m));
+		//Producto de todos
+		System.out.println("Producto de todos= " + (x*y*n*m));
+	}
+
+}
